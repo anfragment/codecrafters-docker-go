7514b23d7f5615045f9b62aa76b6f21e2afb5a6a
grumpydog <me@grumpydog.space>
2023-03-21 17:18:57 +0600

    feat: fs isolation

--- app/main.go
+++ app/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"path"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -13,24 +14,32 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		log.Fatal("tmpDir: ", err)
+	}
+	cmdPath := path.Join(tmpDir, command)
+
+	if err := exec.Command("mkdir", "-p", path.Dir(cmdPath)).Run(); err != nil {
+		log.Fatal("mkdir: ", err)
+	}
+	if err := exec.Command("cp", "-f", command, cmdPath).Run(); err != nil {
+		log.Fatal("cp: ", err)
+	}
+
+	syscall.Chroot(tmpDir)
+	os.Chdir("/")
 	cmd := exec.Command(command, args...)
 
-	stderrPipe, err := cmd.StderrPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stderrPipe.Close()
-	go func() {
-		io.Copy(os.Stderr, stderrPipe)
-	}()
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
 
-	output, err := cmd.Output()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
 			os.Exit(exitErr.ExitCode())
 		} else {
 			log.Fatal(err)
 		}
 	}
-	fmt.Fprint(os.Stdout, string(output))
 }
